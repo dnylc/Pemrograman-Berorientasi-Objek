d54e2389c323a55df2d2f7ead89866c77d0f5db8
DwiNaylaCintia <d1041241017@student.untan.ac.id>
2026-02-12 14:36:20 +0700

    Pertemuan3

---
+++ Pertemuan3/p022WorkingVariabel.java
@@ -0,0 +1,21 @@
+package Pertemuan3;
+
+public class p022WorkingVariabel {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        // Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        // Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+
+}
+// Nama : Dwi Nayla Cintia
+// NIM : D1041241017
+// Tanggal : 12 Februari 2026
